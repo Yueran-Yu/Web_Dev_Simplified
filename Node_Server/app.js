cee141279c0f9c48e5bbee17c385848c0e9a3883
GraceYu <librayuyueran@gmail.com>
2020-12-09 09:23:36 -0600

    the fisrt node server from web dev

--- Node_Server/app.js
+++ Node_Server/app.js
@@ -1,15 +1,25 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
-const server = http.createServer(function(request, response){
-  response.write("Hello")
-  response.end()
+const server = http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('index.html', function (error, data) {
+        if (error) {
+            response.writeHead(404)
+            response.write('Error: File not found')
+        } else {
+            response.write(data)
+        }
+        response.end()
+    })
+
 })
 
-server.listen(port, function(error){
-  if(error){
-    console.log("Something went wrong", error);
-  }else{
-    console.log("Server is listening on port : "
-    + port);
-  }
+server.listen(port, function (error) {
+    if (error) {
+        console.log("Something went wrong", error);
+    } else {
+        console.log("Server is listening on port : "
+            + port);
+    }
 })
